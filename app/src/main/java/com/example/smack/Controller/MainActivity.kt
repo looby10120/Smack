c8f069507a2a8d5c126830237d5b23037705c9be
jedsadakorn.jiraphemounsarb <jedsadakorn.jiraphempunsarb@scb.co.th>
2020-05-22 16:27:24 +0700

    lesson 11

--- app/src/main/java/com/example/smack/Controller/MainActivity.kt
+++ app/src/main/java/com/example/smack/Controller/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.example.smack.Controller
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.smack.R
+import com.example.smack.Services.AuthService
+import com.example.smack.Services.UserDataService
+import com.example.smack.Utilities.BROADCAST_USER_DATA_CHANGED
+import kotlinx.android.synthetic.main.nav_header_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,13 +36,35 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.nav_home,
-            R.id.nav_gallery,
-            R.id.nav_slideshow
-        ), drawerLayout)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home
+            ), drawerLayout
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
 //        navView.setupWithNavController(navController)
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(
+            userDataChangeReceiver, IntentFilter(
+                BROADCAST_USER_DATA_CHANGED
+            )
+        )
+    }
+
+    private val userDataChangeReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (AuthService.isLoggedIn) {
+                UserDataService.apply {
+                    usernameNavHeader.text = name
+                    userEmailNavHeader.text = email
+                    val resourceId =
+                        resources.getIdentifier(avatarName, "drawable", packageName)
+                    userImageNavHeader.setImageResource(resourceId)
+                    userImageNavHeader.setBackgroundColor(returnAvatarColor(avatarColor))
+                    loginBtnNavHeader.text = getString(R.string.logout)
+                }
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -42,8 +73,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun loginBtnNavClicked(view: View) {
-        val loginIntent = Intent(this, LoginActivity::class.java)
-        startActivity(loginIntent)
+        if (AuthService.isLoggedIn) {
+            UserDataService.logout()
+            loginBtnNavHeader.text = getString(R.string.login)
+            usernameNavHeader.text = getString(R.string.login)
+            userEmailNavHeader.text = ""
+            userImageNavHeader.setImageResource(R.drawable.profiledefault)
+            userImageNavHeader.setBackgroundColor(Color.TRANSPARENT)
+        } else {
+            val loginIntent = Intent(this, LoginActivity::class.java)
+            startActivity(loginIntent)
+        }
     }
 
     fun addChannelClicked(view: View) {
--- app/src/main/java/com/example/smack/Services/UserDataService.kt
+++ app/src/main/java/com/example/smack/Services/UserDataService.kt
@@ -1,5 +1,8 @@
 package com.example.smack.Services
 
+import android.graphics.Color
+import java.util.*
+
 object UserDataService {
 
     var id = ""
@@ -8,5 +11,38 @@ object UserDataService {
     var email = ""
     var name = ""
 
+    fun returnAvatarColor(components: String) : Int {
+        val strippedColor = components
+            .replace("$", "")
+            .replace("[", "")
+            .replace("]", "")
+            .replace(",", "")
+
+        var red = 0
+        var green = 0
+        var blue = 0
+
+        val scanner = Scanner(strippedColor)
+        if (scanner.hasNext()) {
+            red = (scanner.nextDouble() * 255).toInt()
+            green = (scanner.nextDouble() * 255).toInt()
+            blue = (scanner.nextDouble() * 255).toInt()
+        }
+
+        return Color.rgb(red, green, blue)
+    }
+
+    fun logout() {
+        id = ""
+        avatarName = ""
+        avatarColor = ""
+        name = ""
+        email = ""
+        AuthService.apply {
+            authToken = ""
+            userEmail = ""
+            isLoggedIn = false
+        }
+    }
 
 }
